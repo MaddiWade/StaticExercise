017ec2016a922a44d56ecc50839b744c59dd9f0d
Maddi Wade <Madelinenwade@gmail.com>
2022-02-24 10:29:33 -0500

    Add project files.

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           var celcius = TempConverter.FarenheitToCelsius(56);
+            Console.WriteLine(celcius);
+
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(28);
+            Console.WriteLine(fahrenheit);
+        }
+
+    }
+
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FarenheitToCelsius(double fTemp) => (fTemp - 32) / 1.8000;
+        public static double CelsiusToFahrenheit(double cTemp) => (cTemp * 9 / 5) + 32;
+
+
+
+
+        
+
+
+    }
+}
+
